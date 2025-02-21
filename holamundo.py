20e15b8c27bd28027c2d9ca93b99ecdbc144c990
aza9603 <azxv9603@gmail.com>
2025-02-20 19:22:35 -0600

    Subida de proyecto de prueba

---
+++ holamundo.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Título de la app
+st.title('¡Hola Mundo con Streamlit!')
+
+# Texto simple
+st.write("Este es un ejemplo básico de cómo usar Streamlit para mostrar una página web.")
