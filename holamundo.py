85dafe3320218ba8727b40bc31c484d5632bd3e2
aza9603 <azxv9603@gmail.com>
2025-02-21 11:20:45 -0600

    modificacion de hola mundo

--- holamundo.py
+++ holamundo.py
@@ -4,4 +4,4 @@ import streamlit as st
 st.title('¡Hola Mundo con Streamlit!')
 
 # Texto simple
-st.write("Este es un ejemplo básico de cómo usar Streamlit para mostrar una página web por Azael Xochihua Vergara.")
+st.write("Este es un ejemplo de cómo usar Streamlit para mostrar una página web por Azael Xochihua Vergara del Instituto Tecnologico de Apizaco.")
